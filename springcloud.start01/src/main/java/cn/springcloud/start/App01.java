fb13a5a6f9ce49d0c6161850ce604a56814edd0f
359442094 <359442094@qq.com>
2019-11-21 13:50:45 +0800

    优化代码

--- springcloud.start01/src/main/java/cn/springcloud/start/App01.java
+++ springcloud.start01/src/main/java/cn/springcloud/start/App01.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
